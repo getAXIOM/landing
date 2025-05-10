83d58978d0e39a26239bb33721f5d28727b36140
tf7software <rhenrywarren@gmail.com>
2025-05-10 13:19:07 -0700

    Updating footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Mail } from 'lucide-react';
-import logoSrc from '../img/whiteLogo.svg';
+import searchLogo from '../img/white.png';
 
 const Footer: React.FC = () => {
   return (
@@ -9,21 +9,16 @@ const Footer: React.FC = () => {
         <div className="flex flex-col md:flex-row justify-between items-center gap-4">
           <div className="flex items-center">
             <img
-       src={logoSrc}
-          className="
-            absolute inset-0
-            w-1/2 max-w-lg mx-auto
-            opacity-10 pointer-events-none
-             animate-float z-0
-            "
-            alt="AXIOM Logo"
-        />
+              src={searchLogo}
+              alt="AXIOM Search logo"
+              className="h-6 w-auto mr-2"
+            />
             <span className="text-sm text-gray-400">
               &copy; {new Date().getFullYear()} AXIOM Search. All rights reserved.
             </span>
           </div>
-          
-          <a 
+
+          <a
             href="mailto:inquire@useaxiom.net"
             className="flex items-center gap-2 text-gray-400 hover:text-white transition-colors duration-300"
           >
@@ -36,4 +31,4 @@ const Footer: React.FC = () => {
   );
 };
 
-export default Footer
+export default Footer;
